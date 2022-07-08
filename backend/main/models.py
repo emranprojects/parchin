46d03cc4c1c912b93537786e9747840c2433a95c
Emran Batmanghelich <emran.bm@gmail.com>
2022-07-08 19:11:55 +0430

    Feat[back]: Add UserDetail and Post models

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
+from djongo.models import ArrayReferenceField
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    isBuyRequest = models.BooleanField(default=False)
+
+
+class UserDetail(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='detail',
+    )
+    phone_number = models.CharField()
+    friends = ArrayReferenceField(
+        to=User,
+        on_delete=models.CASCADE,
+    )
+    posts = ArrayReferenceField(
+        to=Post,
+        on_delete=models.CASCADE,
+    )
