c7ea1c4fa70e217ee52642f2cb5ea124ad8e0274
Emran Batmanghelich <emran.bm@gmail.com>
2022-07-28 17:44:04 +0430

    Feat[front]: Add search link to navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,6 +7,7 @@ import {faSignOutAlt,} from '@fortawesome/free-solid-svg-icons'
 import LoginContext from "./LoginContext";
 import githubIcon from "../static-media/github-logo-32.png"
 import Container from "react-bootstrap/Container";
+import {faSearch} from "@fortawesome/free-solid-svg-icons/faSearch";
 
 export default function Navbar() {
 
@@ -20,7 +21,7 @@ export default function Navbar() {
             {loginContext =>
                 <BootstrapNavbar className="navbar ml-auto p-2" bg="dark" expand="lg">
                     <Container fluid={true}>
-                        <BootstrapNavbar.Brand href="/" className="navbar-dark">پرچین</BootstrapNavbar.Brand>
+                        <BootstrapNavbar.Brand href="/" className="navbar-dark ms-3">پرچین</BootstrapNavbar.Brand>
                         <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" className="navbar-dark"/>
                         <BootstrapNavbar.Collapse id="basic-navbar-nav">
                             <Nav className="container-fluid justify-content-start">
@@ -28,8 +29,15 @@ export default function Navbar() {
                                         <Nav.Item>
                                             <a href={appPaths.userProfile("self")}>
                                                 <img style={{maxHeight: "40px"}}
-                                                     className="rounded-5"
+                                                     className="rounded-5 ms-3 mt-1"
                                                      src="https://quera.org/media/CACHE/images/public/careers/quotes/narrator/a5bcbbf298624df4991db9334ed4f571/7c9bc808882105cb8cd3f1e11387eaff.jpg"/>
+                                            </a>
+                                        </Nav.Item>
+                                        <Nav.Item>
+                                            <a href={appPaths.userSearch} className="btn btn-dark">
+                                                <abbr title="جستجوی کاربران">
+                                                    <FontAwesomeIcon icon={faSearch}/>
+                                                </abbr>
                                             </a>
                                         </Nav.Item>
                                         <Nav.Item>
